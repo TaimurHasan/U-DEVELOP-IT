804de9d71a3d1b724d0cf65f2019d6fe33ee3c9b
TaimurHasan <taimurhasan11@gmail.com>
2022-05-03 21:37:28 -0400

    modularizing code and adding routes

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: process.env.password,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
+
+module.exports = db;
